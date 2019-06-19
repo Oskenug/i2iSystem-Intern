c82915627199de066248af05db1ae8b8d9ca8a6e
Özge Nur <ozge.nur1997@hotmail.com>
2019-06-19 10:11:46 +0300

    Create docker.sql
    refactoring made basic example

---
+++ docker.sql
@@ -0,0 +1,37 @@
+
+SELECT * FROM INTERN;
+
+INSERT INTO INTERN (ID, FIRST_NAME, LAST_NAME, UNIVERSITY, DEPARTMENT, CLASS, CITY) 
+VALUES (1, 'Tolga', 'Çatalpýnar', 'Bilkent Uni','Bilgisayar Mühendisliði', 2, 'Ankara');
+
+INSERT INTO INTERN (ID, FIRST_NAME, LAST_NAME, UNIVERSITY, DEPARTMENT, CLASS, CITY) 
+VALUES (2, 'Ayþegül', 'Karahançer', 'Yýldýz Teknik Üniversitesi','Bilgisayar Öðretmenliði', 3, 'Kayseri');
+
+INSERT INTO INTERN (ID, FIRST_NAME, LAST_NAME, UNIVERSITY, DEPARTMENT, CLASS, CITY)
+VALUES (3, 'Gizemnur', 'Taþkýn', 'Ýstanbul Teknik Üniversitesi','Biliþim Mühendisliði', 3, 'Ýstanbul');
+
+INSERT INTO INTERN (ID, FIRST_NAME, LAST_NAME, UNIVERSITY, DEPARTMENT, CLASS, CITY) 
+VALUES (4, 'Melih Sinan', 'Doðrul', 'Yýldýz Teknik Üniversitesi','Bilgisayar Mühendisliði', 3, 'Trabzon');
+
+INSERT INTO INTERN (ID, FIRST_NAME, LAST_NAME, UNIVERSITY, DEPARTMENT, CLASS, CITY) 
+VALUES (5, 'Servet', 'Tartar', 'Munzur Üniversitesi','Bilgisayar Mühendisliði', 4, 'Mersin');
+
+INSERT INTO INTERN (ID, FIRST_NAME, LAST_NAME, UNIVERSITY, DEPARTMENT, CLASS, CITY) 
+VALUES (6, 'Eren', 'Yalçýn', 'Bilkent Uni','Bilgisayar Mühendisliði', 3, 'Ankara');
+
+INSERT INTO INTERN (ID, FIRST_NAME, LAST_NAME, UNIVERSITY, DEPARTMENT, CLASS, CITY) 
+VALUES (7, 'Yunus', 'Arslan', 'Odtü','Bilgisayar Mühendisliði', 2, 'Antalya');
+
+INSERT INTO INTERN (ID, FIRST_NAME, LAST_NAME, UNIVERSITY, DEPARTMENT, CLASS, CITY) 
+VALUES (8, 'Ümit', 'Atýlgan', 'Yýldýz Teknik Üniversitesi ','Matematik Mühendisliði', 4, 'Çorum');
+
+INSERT INTO INTERN (ID, FIRST_NAME, LAST_NAME, UNIVERSITY, DEPARTMENT, CLASS, CITY) 
+VALUES (9, 'Beytullah', 'Atik', 'Karabük Üniversitesi','Bilgisayar Mühendisliði', 3, 'Edirne');
+
+INSERT INTO INTERN (ID, FIRST_NAME, LAST_NAME, UNIVERSITY, DEPARTMENT, CLASS, CITY) 
+VALUES (10, 'Davut', 'Kurt', 'Ýnönü Üniversitesi','Bilgisayar Mühendisliði', 4, 'Malatya');
+
+INSERT INTO INTERN (ID, FIRST_NAME, LAST_NAME, UNIVERSITY, DEPARTMENT, CLASS, CITY) 
+VALUES (11, 'Özge Nur', 'Koç', 'Trakya Üniversitesi','Bilgisayar Mühendisliði', 4, 'Afyonkarahisar');
+
+
